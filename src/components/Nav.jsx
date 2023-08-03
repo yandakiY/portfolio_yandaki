987d88ad957d52a7d20efb0584a29936e8f8cde1
yandaki <boayves@gmail.com>
2023-08-03 00:03:26 +0000

    add link to pdf files

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,12 +5,11 @@ function Nav() {
   return (
     <nav 
       className='flex flex-row justify-between items-baseline border-b mx-4 my-2'
-      // style={{display:'flex',justifyContent:'space-between',alignItems:'flex-end'}}
     >
         <span className='text-xl sm:text-5xl mb-4'>Yandaki.dev</span>
         <ul className="mb-4 flex flex-row">
             <li className='list-none p-2.5 border border-black rounded-lg text-black flex flex-row items-center hover:shadow-lg hover:shadow-black transition duration-500 ease-out hover:ease-in cursor-pointer'>
-              <a href="mycv.pdf" download>My Resume</a>
+              <a href="/files/mycv.pdf" target="_blank" download>My Resume</a>
               <span className="material-symbols-outlined">
                   download
               </span>
